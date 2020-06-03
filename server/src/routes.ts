d1c673205770f0eb3cf0614b46bdd485030ba827
aryclenio <aryclenio2010@gmail.com>
2020-06-02 23:07:53 -0300

    :rocket: Initializing seeds

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import knex from "./database/connection";
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  return res.json({});
+routes.get("/items", async (req, res) => {
+  const items = await knex("items").select("*");
+  const serializedItems = items.map((item) => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+  return res.json(serializedItems);
 });
 
 export default routes;
