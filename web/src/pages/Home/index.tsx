cc27ee57d868c318cd67835d1b98f6dca32ada6e
aryclenio <aryclenio2010@gmail.com>
2020-06-06 16:29:02 -0300

    :rocket: Implementing form

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 import { FiLogIn } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -16,12 +17,12 @@ const Home = () => {
             Ajudamos as pessias a encontrarem pontos de coleta da forma mais
             legal possivel
           </p>
-          <a href="google.com">
+          <Link to="create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um novo ponto de coleta.</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -6,7 +6,8 @@ import CreatePoint from "./pages/CreatePoint";
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Route component={Home} path="/" />
+      <Route component={Home} path="/" exact />
+      <Route component={CreatePoint} path="/create-point" />
     </BrowserRouter>
   );
 };
